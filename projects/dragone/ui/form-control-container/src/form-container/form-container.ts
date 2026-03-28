c52583b3b35ab012092ab944122ac105a54cb453
Marco Rinaldi <marco.rinaldi9@live.com>
2026-03-28 16:26:27 +0100

    feat: implemented required label check and animation errors

--- projects/dragone/ui/form-control-container/src/form-container/form-container.ts
+++ projects/dragone/ui/form-control-container/src/form-container/form-container.ts
@@ -4,7 +4,6 @@ import {
   Component,
   computed,
   contentChild,
-  effect,
   input,
 } from '@angular/core';
 import { FORM_FIELD } from '@angular/forms/signals';
@@ -21,7 +20,10 @@ import { ValidationError } from '../validation-error/validation-error';
     <div class="field-container">
       @if (label()) {
         <!-- eslint-disable-next-line @angular-eslint/template/label-has-associated-control -->
-        <label class="drgn-label-md-600 label" drgnLabel>{{ label() }}</label>
+        <label class="drgn-label-md-600 label" drgnLabel>
+          @let l = label() + (isRequired() ? '*' : '');
+          {{ l }}
+        </label>
       }
       <div class="control">
         <ng-content select="[formField]"> Errata configurazione </ng-content>
@@ -34,6 +36,8 @@ import { ValidationError } from '../validation-error/validation-error';
       <span
         class="error-message drgn-helper-text-xs-400"
         drgnError
+        animate.enter="error-enter"
+        animate.leave="error-leave"
         [attr.role]="$first ? 'alert' : null"
         [validator]="error.kind"
         >{{ error.message }}</span
@@ -52,21 +56,8 @@ export class FormContainer {
   readonly label = input<string>();
   readonly description = input<string>();
   readonly inline = input(false, { transform: booleanAttribute });
-  readonly validations = input<
-    | 'required'
-    | 'min'
-    | 'max'
-    | 'minLength'
-    | 'maxLength'
-    | 'pattern'
-    | 'email'
-    | (string & Record<never, never>[])
-  >();
-  private readonly formField = contentChild(FORM_FIELD);
+  private readonly formField = contentChild.required(FORM_FIELD);
 
-  protected errors = computed(() => this.formField()?.errors());
-
-  constructor() {
-    effect(() => console.log(this.formField()?.errors()));
-  }
+  protected errors = computed(() => this.formField().errors());
+  protected isRequired = computed(() => this.formField().state().required());
 }
