ca440250fd6e45a3fb33ce420c8b900741f8ddb8
Marco Rinaldi <marco.rinaldi9@live.com>
2026-08-08 17:07:57 +0200

    feat(ui): add notifier and to-value utility functions

--- projects/dragone/ui/utils/public-api.ts
+++ projects/dragone/ui/utils/public-api.ts
@@ -4,5 +4,7 @@ export * from './src/event-plugins/provide-event-plugins';
 export * from './src/types/branding';
 export * from './src/types/svg-icon';
 export * from './src/predicates/is-nil';
+export * from './src/predicates/notifier';
+export * from './src/predicates/to-value';
 export * from './src/dom/to-element';
 export * from './src/theming/theme';
---
+++ projects/dragone/ui/utils/src/predicates/to-value.ts
@@ -0,0 +1,21 @@
+import { isSignal, untracked as untr } from '@angular/core';
+
+import type { MaybeSignal } from '../types/branding';
+
+export interface ToValueFn {
+  <T>(val: MaybeSignal<T>): T;
+  untracked: <T>(val: MaybeSignal<T>) => T;
+}
+
+const toValueFn = <T>(maybeSignal: MaybeSignal<T>, untracked = false): T => {
+  if (isSignal(maybeSignal)) {
+    return untracked ? untr(maybeSignal) : maybeSignal();
+  }
+  return maybeSignal;
+};
+
+export const toValue = ((): ToValueFn => {
+  const fn = toValueFn as ToValueFn;
+  fn.untracked = <T>(val: MaybeSignal<T>): T => toValueFn(val, true);
+  return fn;
+})();
