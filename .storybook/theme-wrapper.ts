175401eb1937372b314b8df54c3a174d2f0b7686
Marco Rinaldi <marco.rinaldi9@live.com>
2026-08-21 23:24:45 +0200

    refactor(storybook): update theme handling

--- .storybook/theme-wrapper.ts
+++ .storybook/theme-wrapper.ts
@@ -1,22 +1,25 @@
 import { Component, effect, input } from '@angular/core';
 
+import { provideThemeState, themeFactory } from '../projects/dragone/ui/utils';
+
 @Component({
   host: {
-    '[class.drgn-dark]': 'darkMode() === "dark"',
+    '[attr.data-theme]': 'state.resolvedTheme() ? state.resolvedTheme() : null',
     '[style.align-items]': '"center"',
-    '[style.background-color]': 'darkMode() === "dark" ? "#002460" : "#FFFFFF"',
+    '[style.background-color]': 'state.resolvedTheme() === "dark" ? "#002460" : "#FFFFFF"',
     '[style.display]': '"flex"',
     '[style.justify-content]': '"center"',
     '[style.padding]': '"2rem"',
     '[style.transition]': '"background-color 0.3s"',
   },
+  providers: [provideThemeState({ inherit: false })],
   selector: 'theme-wrapper',
   template: `<ng-content />`,
 })
 export class ThemeWrapper {
-  readonly darkMode = input<'light' | 'dark'>('light');
+  readonly darkMode = input<'light' | 'dark' | undefined>();
   readonly ngDevMode = input<boolean, 'true' | 'false'>(true, { transform: val => val === 'true' });
-
+  readonly state = themeFactory({ theme: this.darkMode });
   constructor() {
     effect(() => {
       const isDevMode = this.ngDevMode();
